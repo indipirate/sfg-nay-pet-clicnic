8235cfa27b6d50a1864bd2ca037db079a7c69c46
nayeem <nayeem4edu@gmail.com>
2019-07-21 21:43:09 +0530

    project setup

---
+++ src/main/java/com/sfg/nay/pet/clinic/SfgNayPetClicnicApplication.java
@@ -0,0 +1,13 @@
+package com.sfg.nay.pet.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgNayPetClicnicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgNayPetClicnicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sfg/nay/pet/clinic/SfgNayPetClicnicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sfg.nay.pet.clinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SfgNayPetClicnicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
